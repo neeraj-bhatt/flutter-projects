ddfa29c51f3e702f63da63334bf7b227952b2243
Neeraj <neerajbhatt264@gmail.com>
2024-12-30 18:55:55 +0530

    place-detail screen added

--- favorite_places/lib/widgets/places_list.dart
+++ favorite_places/lib/widgets/places_list.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screens/place_detail_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:favorite_places/models/place.dart';
@@ -10,16 +11,26 @@ class PlacesList extends StatelessWidget {
   Widget build(BuildContext context) {
     if (places.isEmpty) {
       return Center(
-        child: Text('Nothing to Show', style: Theme.of(context)
-            .textTheme
-            .bodyLarge!
-            .copyWith(color: Theme.of(context).colorScheme.onSurface),),
+        child: Text(
+          'Nothing to Show',
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onSurface),
+        ),
       );
     }
 
     return ListView.builder(
       itemCount: places.length,
       itemBuilder: (ctx, index) => ListTile(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (ctx) => PlaceDetailScreen(place: places[index]),
+              ));
+        },
         title: Text(
           places[index].title,
           style: Theme.of(context)
