25e62a650d937f915670beaa0c6baa3b062399f0
Neeraj <neerajbhatt264@gmail.com>
2025-01-20 12:21:37 +0530

    firebase config for authentication (chit-chat)

---
+++ chit_chat/lib/screens/auth.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() {
+    return _AuthScreenState();
+  }
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  var _isLogIn = true;
+  final _formKey = GlobalKey<FormState>();
+  var _enteredEmail;
+  var _enteredPassword;
+
+  void _submit(){
+    if(!_formKey.currentState!.validate()) return;
+    _formKey.currentState!.save();
+    print(_enteredEmail);
+    print(_enteredPassword);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                    top: 30.0, left: 20.0, right: 20.0, bottom: 20.0),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              ),
+              Card(
+                margin: const EdgeInsets.all(20.0),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(labelText: 'Email'),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter an email address';
+                              }
+                              if (!RegExp(
+                                      r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+                                  .hasMatch(value)) {
+                                return 'Enter valid email address';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) => _enteredEmail = newValue,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter a password';
+                              }
+                              if (!RegExp(
+                                      r'^(?=.*\d)[A-Za-z\d]{8,}$')
+                                  .hasMatch(value)) {
+                                return 'Password must be at least 8 character long, include a letter and a number';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) => _enteredPassword = newValue,
+                          ),
+                          const SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed: () {
+                              _submit();
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer),
+                            child: Text(_isLogIn ? 'Log in' : 'Sign Up'),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogIn = !_isLogIn;
+                                });
+                              },
+                              child: Text(_isLogIn
+                                  ? 'Create new Account'
+                                  : 'Already have an Account?'))
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
